90197cf4cdda280f358f53e13d23444b18f302cf
nesicmarija <marijanesic8755@gmail.com>
2021-05-10 22:30:34 +0200

    PrintResult funtion added

--- v1/z1/main.c
+++ v1/z1/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+
+void PrintResult(int res)
+{
+    printf("Result: %d\n", res);
+}
+
 int main()
 {
-int a = 3;
-int b = 8;
-int c;
-if(a > b)
-c = a - b;
-else
-c = b - a;
-printf("Result: %d\n", c);
-return 0;
+    int a = 3;
+    int b = 8;
+    int c;
+
+    if(a > b)
+      c = a - b;
+    else
+      c = b - a;
+
+    PrintResult(c);
+	
+    return 0;
 }
