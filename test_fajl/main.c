1ab0a5d7d3597fd07af05e63dd9be4e74bcc9dfd
nesicmarija <marijanesic8755@gmail.com>
2021-05-13 18:34:45 +0200

    dodajem fajl

---
+++ test_fajl/main.c
@@ -0,0 +1,25 @@
+/**
+* @file main.c
+* @brief opis
+* @author Marija Nešiæ
+* @date 11-5-2021
+* @version 1.0
+*/
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+   {
+	DDRB |=1<<5;//PB5 je izlaz
+
+	while(1)
+	{
+		PORTB |=1<<5;//LED ON
+		_delay_ms(1000);//pauza 1s
+		PORTB &=~(1<<5);//LED OFF
+		_delay_ms(1000);//pauza 1s
+	}
+	return 0;
+   }
+
